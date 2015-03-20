7cb716d40a5da0e24a9e9c1b1765e6a172dda6da
Xianheng Ma <xianheng.ma@gmail.com>
2015-03-19 20:19:40 -0700

    make it compilable

--- src/main/java/com/d2fn/guano/DumpJob.java
+++ src/main/java/com/d2fn/guano/DumpJob.java
@@ -6,6 +6,9 @@ import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.data.Stat;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 /**
